2a7312cbacacf0dbbad0ab3fb3792b34d2639e2e
Alessandro <luisialessandro95@gmail.com>
2022-01-11 23:23:42 +0100

    about-first-part

--- src/components/macro/Main.vue
+++ src/components/macro/Main.vue
@@ -1,21 +1,22 @@
 <template>
     <main>
         <Hero/>
+        <About/>
     </main>
 </template>
 
 <script>
 import Hero from '../sections/meinSection/Hero.vue'
+import About from '../sections/meinSection/About.vue'
 export default {
     name: 'Main',
     components:{
         Hero,
+        About
     }
 }
 </script>
 
 <style lang="scss" scoped>
-    h3{
-        color: red;
-    }
+
 </style>
---
+++ src/components/sections/meinSection/About.vue
@@ -0,0 +1,38 @@
+<template>
+  <section id="about">
+      <div class="container">
+        <div class="about-title">
+            <img src="../../../assets/images/page-logo-1.png" alt="">
+            <h2>Inspired By Excellence & Innovation</h2>
+            <p>We offere a wide range of hight quality of teaching and extra-curricula activities.</p>
+            <hr>
+        </div>
+      </div>
+  </section>
+</template>
+
+<script>
+export default {
+    name: 'About'
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../../../assets/style/partials/variables.scss';
+.about-title{
+    text-align: center;
+    img{
+        margin: 70px 0 30px;
+    }
+    p{
+        color: #94979f;
+        margin: 30px 0;
+    }
+    hr{
+        width: 20%;
+        margin: 0 auto;
+        color: $first-color;
+    }
+    // background-color: $second-color;
+}
+</style>
