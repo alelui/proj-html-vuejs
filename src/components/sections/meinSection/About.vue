c93c3d9d6b967d5f531bf70ddcd6951791be4d40
Alessandro <luisialessandro95@gmail.com>
2022-01-12 00:04:30 +0100

    about

--- src/components/sections/meinSection/About.vue
+++ src/components/sections/meinSection/About.vue
@@ -2,10 +2,21 @@
   <section id="about">
       <div class="container">
         <div class="about-title">
-            <img src="../../../assets/images/page-logo-1.png" alt="">
+            <img class="logo" src="../../../assets/images/page-logo-1.png" alt="">
             <h2>Inspired By Excellence & Innovation</h2>
             <p>We offere a wide range of hight quality of teaching and extra-curricula activities.</p>
             <hr>
+            <div class="about-main">
+                <div class="about-main__text">
+                    <h2>why chose avanda university</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minima in odio, esse iste hic tenetur excepturi amet sit, eveniet accusamus iure commodi libero consequatur sed dolores ratione soluta odit eligendi!</p>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minima in odio, esse iste hic tenetur excepturi amet sit, eveniet accusamus iure commodi libero consequatur sed dolores ratione soluta odit eligendi!</p>                            
+
+                </div>
+                <div class="about-main__video">
+                    <img src="../../../assets/images/business_seminar_event_feat-600x402.jpg" alt="">
+                </div>
+            </div>
         </div>
       </div>
   </section>
@@ -21,7 +32,7 @@ export default {
 @import '../../../assets/style/partials/variables.scss';
 .about-title{
     text-align: center;
-    img{
+    .logo{
         margin: 70px 0 30px;
     }
     p{
@@ -32,6 +43,27 @@ export default {
         width: 20%;
         margin: 0 auto;
         color: $first-color;
+    }
+    .about-main{
+        width: 100%;
+        display: flex;
+        margin: 50px 0;
+
+        &__text, &__video{
+            width: 50%;
+        }
+        &__text{
+            text-align: left;
+            h2{
+                text-transform: uppercase;
+            }
+            p{
+                font-size: 14px;
+            }
+        }
+        &__video img{
+            width: 70%;
+        }
     }
     // background-color: $second-color;
 }
