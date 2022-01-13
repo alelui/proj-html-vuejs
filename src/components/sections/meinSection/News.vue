593d2d3968e078a272ea84d855f5941be0059ae3
Alessandro <luisialessandro95@gmail.com>
2022-01-13 01:05:19 +0100

    news2

--- src/components/sections/meinSection/News.vue
+++ src/components/sections/meinSection/News.vue
@@ -13,14 +13,48 @@
                     </div>
                     <div class="box-news__title">
                         <h2>What do successful grads think you should study?</h2>
-                        <p>By <span>admin</span>|March 16th, 2016</p>
+                        <p><span>By</span> admin <span>|March 16th, 2016</span></p>
                     </div>
                     <div class="box-news__info">
                         <div>
-                            <span>Read More></span>
+                            <span>Read More > </span>
                         </div>
                         <div>
-                            <i class="far fa-comments"></i><span>0</span>
+                            <i class="far fa-comments"></i><span> 0</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="box-news">
+                    <div class="box-news__boxImg">
+                        <img src="../../../assets/images/news_single_post_header_2-400x222.jpg" alt="">
+                    </div>
+                    <div class="box-news__title">
+                        <h2>Former student discusses success in the fashion industry</h2>
+                        <p><span>By</span> admin <span>|March 16th, 2016</span></p>
+                    </div>
+                    <div class="box-news__info">
+                        <div>
+                            <span>Read More > </span>
+                        </div>
+                        <div>
+                            <i class="far fa-comments"></i><span> 0</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="box-news">
+                    <div class="box-news__boxImg">
+                        <img src="../../../assets/images/news_single_post_header_3-400x222.jpg" alt="">
+                    </div>
+                    <div class="box-news__title">
+                        <h2>How do you best prepare for university?</h2>
+                        <p><span>By</span> admin <span>|March 16th, 2016</span></p>
+                    </div>
+                    <div class="box-news__info">
+                        <div>
+                            <span>Read More > </span>
+                        </div>
+                        <div>
+                            <i class="far fa-comments"></i><span> 0</span>
                         </div>
                     </div>
                 </div>
@@ -60,14 +94,39 @@ export default {
         width: 100%;
         display: flex;
         text-align: left;
-    .box-news{
-        background-color: white;
-        width: calc(100% / 3);
-        margin: 60px 0 150px;
-        &__boxImg img{
-            width: 100%;
+        
+        .box-news{
+            background-color: white;
+            width: calc(100% / 3 - 20px);
+            margin: 60px 10px 150px;
+            color: $first-color;
+
+            &__title,&__info{
+                padding: 10px;
+            }
+
+            &__boxImg img{
+                width: 100%;
+            }
+            &__title{
+                h2{
+                    margin: 20px 0 5px;
+                    font-size: 20px;
+                }
+                p{
+                    margin: 0 0 30px;
+                    color: $first-color;
+                    span{
+                        color: black;
+                    }
+                }
+            }
+            &__info{
+                display: flex;
+                justify-content: space-between;
+                position: relative;
+            }
         }
-    }
     }
 }
 </style>
