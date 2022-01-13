02a5f541e0a0cd1e294702f5545644d12212f924
Alessandro <luisialessandro95@gmail.com>
2022-01-13 15:32:36 +0100

    4 on five

--- src/components/sections/FooterTop.vue
+++ src/components/sections/FooterTop.vue
@@ -11,9 +11,54 @@
             <i class="far fa-clock"></i><span>Monday – Friday: 8:00 AM – 10:00 PM</span>
         </div>
       </div>
-      <div  class="footer-box"></div>
-      <div class="footer-box"></div>
-      <div class="footer-box"></div>
+      <div  class="footer-box">
+            <h3>RECENT POSTS</h3>
+            <div>
+                <i class="fas fa-chevron-right"></i><span>What do successful grads think you should study?</span>
+            </div>
+            <hr>
+            <div>
+                <i class="fas fa-chevron-right"></i><span>Former student discusses success in the fashion industry</span>
+            </div>
+            <hr>
+            <div>
+                <i class="fas fa-chevron-right"></i><span>How do you best prepare for university?</span>
+            </div>
+            <hr>
+      </div>
+      <div class="footer-box">
+        <h3>COURESE</h3>
+        <ul>
+            <li>Arts & Humanities</li>
+            <li>Buoisness & Managment</li>
+            <li>Engeniare & Tecnology</li>
+            <li>Life Sience & Medicine</li>
+            <li>Natura Sience</li>
+        </ul>
+        <h3>ADMISSION</h3>
+        <ul>
+            <li>Eligibility</li>
+            <li>Application</li>
+            <li>Review Proces</li>
+            <li>How to play</li>
+        </ul>
+      </div>
+      <div class="footer-box">
+        <h3>Resources</h3>
+        <ul>
+            <li>Campus Directory</li>
+            <li>Accademic calendar</li>
+            <li>Spoprt Clubs</li>
+            <li>Graduate Programs</li>
+            <li>Faculty & Staff</li>
+        </ul>
+        <h3>QUICK LINKS</h3>
+        <ul>
+            <li>Universitary Library</li>
+            <li>Campus Servise</li>
+            <li>Carrresrs</li>
+        </ul>
+      </div>
       <div class="footer-box"></div>
   </section>
 </template>
@@ -35,22 +80,35 @@ export default {
         background-position: center;
         font-size: 13px;
         padding: 50px;
-        .fa-home, .fa-clock, .fa-phone, .fa-envelope{
+        .fa-home, .fa-clock, .fa-phone, .fa-envelope, .fa-chevron-right{
             margin-right: 10px;
         }
+        h3{
+            margin: 20px 0;
+        }
+
         div:nth-of-type(1){
-            h3{
-                margin-bottom: 25px;
-            }
             div{
                 display: flex;
                 .fa-home, .fa-clock{
                     position: relative;
-                    top: 8px;
+                    top: 5px;
                 }
             }
-        } 
-        div:nth-of-type(2) {flex-grow: 1;}
+        }
+
+        div:nth-of-type(2) {
+            div{
+                display: flex;
+                    .fa-chevron-right{
+                    position: relative;
+                    top: 5px;
+                }
+            }
+            hr{
+            margin-bottom: 30px;
+            }
+        }
         div:nth-of-type(3) {flex-grow: 1;}
         div:nth-of-type(4) {flex-grow: 1.3;}
         div:nth-of-type(5) {flex-grow: 1.8;}
@@ -59,8 +117,7 @@ export default {
             height: 100%;
             border: 1px solid black;
             padding: 10px;
-            line-height: 30px;
-            
+            line-height: 25px;
         }
     }
 </style>
